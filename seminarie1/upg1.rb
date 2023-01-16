aab1b2f12d623da4955d30715a146c6ec23fab3e
Albion Halilaj <albha882@student.liu.se>
2023-01-16 17:06:15 +0100

    Uppgift 5 har påbörjats, behövs fullname funktion

--- seminarie1/upg1.rb
+++ seminarie1/upg1.rb
@@ -2,7 +2,7 @@
 # Uppgift 1
 # Iterator-funktion som anropar givet block n gånger. 
 def n_times(n, &block)
-    for _ in 1..n 
+    n.times do
         block.call()
     end
 end
@@ -14,7 +14,7 @@ class Repeat
     end
     # Metoden each som tar ett block och anropar antal gånger som angavs när objektet skapades.
     def each(&block)
-        for _ in 1..@n
+        @n.times do
             block.call()
         end
     end
@@ -23,8 +23,37 @@ end
 
 n_times(3) { puts "Hello!" }
 do_three = Repeat.new(3)
-# Skriver ut instansen av Repeat med @n=3
-p do_three
 do_three.each { puts "Horray!" }
 
 # Uppgift 2
+#Factorial är en funktion som beräknar fakutleten av ett positivt tal.
+def factorial(n)
+    (1..n).inject(:*)
+end
+
+factorial(9)
+
+#Uppgift 3
+#Max_by returnerar objektet med maximal värde från angiven block.
+def longest_string(ar)
+    p ar.max_by{|obj|obj.length} 
+end
+
+longest_string ["apelsin", "banan", "citron"]
+
+#Uppgift 4
+def find_it(ar, &block)
+    p ar.find{block}
+end
+
+find_it(["apelsin", "banan", "citron"]) { |a,b| a.length > b.length }
+
+#Uppgift 5
+class PersonName
+    def initialize(name, surname)
+        @name = name
+        @surname = surname
+    end
+    
+
+end
