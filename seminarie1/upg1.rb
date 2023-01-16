22ea5118f7f98d7ba3f26a1b1dd9c4b32c5af490
Giedre Aleksandrauskaite <gieal640@student.liu.se>
2023-01-16 14:33:21 +0100

    class Repeat added

--- seminarie1/upg1.rb
+++ seminarie1/upg1.rb
@@ -1,10 +1,27 @@
 
+# Iterator-funktion som anropar givet block n gånger. 
 def n_times(n, &block)
     for _ in 1..n 
         block.call()
     end
 end
 
+# Iterator-klass som instansieras med ett tal.
+class Repeat
+    def initialize(n)
+        @n = n
+    end
+    # Metoden each som tar ett block och anropar antal gånger som angavs när objektet skapades.
+    def each(&block)
+        for _ in 1..@n
+            block.call()
+        end
+    end
+end
 
 
 n_times(3) { puts "Hello!" }
+do_three = Repeat.new(3)
+# Skriver ut instansen av Repeat med @n=3
+p do_three
+do_three.each { puts "Horray!" }
