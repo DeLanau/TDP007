a1a68767542378508715789aa5a01da3208512c8
Albion Halilaj <albha882@student.liu.se>
2023-01-20 14:22:51 +0100

    sem2 upg1 done

--- seminarie2/sem2.rb
+++ seminarie2/sem2.rb
@@ -1,13 +1,13 @@
 '''Uppgift 1a'''
-def file_to_list(name)
+def file_to_list(name, re)
 
     file = File.open(name).readlines().map()
-
+    re = Regexp.new(re)
     list = []
 
     for f in file do
 
-        if f =~ /\d{2}  -  \d{2}/ then
+        if f =~ re then
             list.push(f)
         end
     end
@@ -15,17 +15,21 @@ def file_to_list(name)
     return list
 end
 
-def sort_team(name) 
+#puts sort_team("football.txt")
 
-    teams = file_to_list(name)
+def sort(name, re) 
+
+    teams = file_to_list(name, re)
+
+    re = Regexp.new(re)
 
     teams.sort_by! do |t|
 
-        t = t.slice(/\d{2}  -  \d{2}/)
+        t = t.slice(re) # "12   -   12" "12    12"
 
         a = t[0..2].to_i
-        b = t[7..9].to_i
-
+        b = t[-4..-1].to_i
+        
         diff = 0
 
         if a < b then
@@ -33,63 +37,13 @@ def sort_team(name)
         else
             diff = a - b 
         end 
-        
     end 
 
     return teams
 end 
 
-puts sort_team("football.txt")
-
-def sort_team(name) 
-
-    teams = file_to_list(name)
-
-    teams.sort_by! do |t|
-
-        t = t.slice(/\d{2}  -  \d{2}/) # "12   -   12"
-
-        a = t[0..2].to_i
-        b = t[7..9].to_i
-
-        diff = 0
-
-        if a < b then
-            diff = b - a 
-        else
-            diff = a - b 
-        end 
-        
-    end 
-
-    return teams
-end 
-
-str = "1  88*    59*    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5"
-
-puts str.slice(/\d{2}\*?    \d{2}\*?/)[0..2].to_i #^(\s*(\d{1,2} || \w{2})\s*)(\d{2}\*?\.?\d*\s{2,4}\d{2}\*?\.?\d*)
+#^(\s*(\d{1,2} || \w{2})\s*)(\d{2}\*?\.?\d*\s{2,4}\d{2}\*?\.?\d*)
 
 #^(\s*\d{1,2} || \s*\w{2}\s*)\K (\d{2}\*?\.?\d*\s{2,4}\d{2}\*?\.?\d*)
+#(/\d{2}  -  \d{2}/
 
-# for f in teams do
-        
-#     if f =~ /\d{2}  -  \d{2}/ then
-
-#         f = f.slice(/\d{2}  -  \d{2}/)
-
-#         a = f[0..2].to_i
-#         b = f[7..9].to_i
-
-#         diff = 0
-
-#         if a < b then
-#             diff = b - a 
-#         else
-#             diff = a - b 
-#         end 
-        
-#         differens.push(diff)
-    
-#     end 
-# end
-# return differens.sort[0]
