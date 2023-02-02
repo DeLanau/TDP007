3455c3019e8a30e52f1c44160fb979b0eeaf91d0
Albion Halilaj <albha882@student.liu.se>
2023-02-02 13:48:46 +0100

    some testing

---
+++ seminarie3/upg1/test_upg1.rb
@@ -0,0 +1,16 @@
+require "./upg1"
+require "./policy"
+require "test/unit"
+
+class MyTest < Test::Unit::TestCase
+    def test_upg1
+        per = Person.new("Volvo", "58435", 2, "M", 32)
+        carl = Person.new("BMW", "58937", 2, "M" , 32)
+        julia = Person.new("Fiat", "58647", 22, "F" , 40)
+        
+        assert_equal(15.66, per.evaluate_policy("policy.rb"))
+        assert_equal(21.15, carl.evaluate_policy("policy.rb"))
+        assert_equal(17, julia.evaluate_policy("policy.rb"))
+
+    end
+end
