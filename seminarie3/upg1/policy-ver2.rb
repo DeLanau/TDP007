5d7022146eb4e868ca3cf10b3e8d97717234f586
Albion Halilaj <albha882@student.liu.se>
2023-02-07 14:20:03 +0100

    added version 2 method missing upg1

---
+++ seminarie3/upg1/policy-ver2.rb
@@ -0,0 +1,31 @@
+BMW 5
+Citroen 4
+Fiat 	3
+Ford 	4
+Mercedes 5
+Nissan 	4
+Opel 	4
+Volvo 	5
+
+zip_58937 	9
+zip_58726 	5
+zip_58647 	3
+
+exp_0_1  	3
+exp_2_3  	4
+exp_4_15  4.5
+exp_16_99 5
+
+F 	1
+M 	1
+
+age_18_20  2.5
+age_21_23  3
+age_24_26 3.5
+age_27_29  4
+age_30_39 4.5
+age_40_64 5
+age_65_70 4
+age_71_99  3
+
+
---
+++ seminarie3/upg1/upg1-ver2.rb
@@ -0,0 +1,72 @@
+class Person
+
+    #car, postnummer, har haft körkort, kön, ålder
+    def initialize(car = '', post = '', exp = '', gender = '', yo = '')
+        @car = car
+        @post = post
+        @exp = exp
+        @gender = gender
+        @yo = yo
+        @points = 0
+    end
+    
+
+    def evaluate_policy(filename)
+        #läser in filen och kör den som ruby kod. 
+        self.instance_eval(File.read(filename))
+        rules()
+        p @points
+    end   
+
+    def method_missing(name, *args)
+        if args.empty?
+            p "#{name} "
+
+        else
+            #p "#{name}  #{args}"
+            if name.to_s == @car then
+                @points += args[0]
+            end
+
+            if name.to_s.split('_')[0] == "zip" and name.to_s.split('_')[1] == @post then
+                @points += args[0]
+            end
+            
+            if name.to_s.split('_')[0] == "exp" then
+                calc_range(name, @exp, args)
+            end
+            
+            if name.to_s.split('_')[0] == @gender then
+                @points += args[0]
+            end
+
+            if name.to_s.split('_')[0] == "age" then
+                calc_range(name, @yo, args)
+            
+            end
+        end
+
+    def rules()
+
+        if @gender == "M" and @exp < 3 then
+            @points *= 0.9
+        end
+          
+        if @car == "Volvo" and @post.match(/^(58)/) then
+            @points *= 1.2
+        end
+
+    end 
+
+    def calc_range(name, type, args)
+        range = name.to_s.split('_')[1].to_i..name.to_s.split('_')[2].to_i
+        if range.include?(type) then
+            @points += args[0]
+        end
+    end
+
+    
+end
+
+per = Person.new("Volvo", "58435", 2, "M", 32)
+per.evaluate_policy("policy.rb")
